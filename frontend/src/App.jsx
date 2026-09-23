d5defedfcaa722cef6fc51b44e74c502d373d5ce
Shantanu-thapa <shantanuthapa021@gmail.com>
2026-09-23 13:37:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -42,8 +42,7 @@ import "./App.css";
 
 const API = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:5000/api/v1",
+    import.meta.env.VITE_API_URL ,
   headers: {
     "Content-Type": "application/json",
   },
